e63a259082ac98668c8780a2e90d13ae882fef7a
sreddy75 <hellosuren@gmail.com>
2024-03-20 09:14:39 +1000

    adding langchain community module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tempfile import NamedTemporaryFile
 from langchain_experimental.agents import create_csv_agent
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
